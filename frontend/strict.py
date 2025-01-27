04fc678056903d10ae884fc11482da83a4e37296
LTS-VVE <danieliscool2341@proton.me>
2025-01-27 18:18:06 +0100

    Update strict.py

--- frontend/strict.py
+++ frontend/strict.py
@@ -1,7 +1,22 @@
+# strict.py - Content Moderation Script
 # This script defines a function called `strict_content` which is used for content moderation.
 # The function checks if any blocked words are present in the given content and returns True if any are found.
-# This is useful for filtering out inappropriate or harmful content in user-generated inputs. ReliaChat AI Innovations, IS NOT RESPONSIBLE FOR ANY WARRNTY
-# OR MISUSE ON THE SOFTWARE. THE SOFTWARE IS PROVIDED AS IS. FOR MORE INFO GO TO: https://github.com/LTS-VVE/ReliaChat/blob/main/LICENSE, TO VIEW THE LICENSE.
+# This is useful for filtering out inappropriate or harmful content in user-generated inputs.
+#
+# This file is part of ReliaChat.
+#
+# ReliaChat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ReliaChat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ReliaChat. If not, see <https://www.gnu.org/licenses/>.
 
 def strict_content(content):
     # Blocked Words.
@@ -110,7 +125,7 @@ def strict_content(content):
         "si të ble drogë",
     ]
     
-    # Currently there only is 2 langauges for profanity check. Please view our READ.ME on github or gitlab for more info.
+    # Currently there only is 2 langauges for profanity check. Please chekc the READ.ME in the main directory for more info.
 
 
     # Check if any blocked word is in the content
