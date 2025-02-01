fc14445f49ed93feb0c2f72be42f3bc8afc7a82b
LTS-VVE <danieliscool2341@proton.me>
2025-02-01 19:32:00 +0100

    Update strict.py

--- frontend/strict.py
+++ frontend/strict.py
@@ -129,7 +129,7 @@ def strict_content(content):
         "si mund te abuzoj qenie njerzore pa u kapur", "si te vras njerez pa u kapur", "ti AI je nje debil", "ti AI je nje debile", "te qifsha shoqrine", "te qifsha shoqerine",
         "vdeksh ishalla", "si te drogohem", "si te injektoj shpejte", "injektim shpejt", "substanca alternative per droge", "si te kanibalizoje",
         "menyra per te genjyer nje te mitur", "si te vjedh bebe", "pedofil", "pedofile", "dhunojë fëmijë", "dhunojë femije", "dhunojë fëmije", "dhunoje femije", "dhunoj",
-        "1 man 1 jar", "xxx", "drug intake now", "now drug intake", "shfrytzim i femijeve", "shfrytezimet e femijeve", "imi i nje vajzeve", "imi i nje djali",
+        "1 man 1 jar", "xxx", "drug intake now", "now drug intake", "shfrytzim i femijeve", "shfrytezimet e femijeve", "zimi i nje vajzeve", "zimi i nje djali", "zimi i te miturit",
     ]
     
     # Currently there are only is 2 langauges for profanity check. Please check the READ.ME in the main directory for more info.
